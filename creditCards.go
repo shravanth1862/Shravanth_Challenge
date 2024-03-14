f64a75486f255912969d06434876116d02035a85
shravanth1862 <52979079+shravanth1862@users.noreply.github.com>
2024-03-14 00:11:32 -0400

    Update creditCards.go

--- creditCards.go
+++ creditCards.go
@@ -7,21 +7,21 @@ import (
 func validateCreditCard(cardNumber string) string {
 
 	if cardNumber[0] != '4' && cardNumber[0] != '5' && cardNumber[0] != '6' { //make sure card starts with either 4 or 5 or 6
-		return "Invalid1"
+		return "Invalid"
 	}
 
 	length := len(cardNumber)
 	containsHyphens := false
 	if length == 19 { //makes sure length is 16 exactly. only allow greter than 16 if hyphens are present at expected indicies
 		if cardNumber[4] != '-' && cardNumber[9] != '-' && cardNumber[14] != '-' {
-			return "Invalid2"
+			return "Invalid"
 		} else {
 			containsHyphens = true
 		}
 	} else if length < 16 {
-		return "Invalid3"
+		return "Invalid"
 	} else if length > 16 {
-		return "Invalid4"
+		return "Invalid"
 	}
 
 	for i := 0; i < len(cardNumber); i++ {
@@ -29,14 +29,14 @@ func validateCreditCard(cardNumber string) string {
 			if containsHyphens == true && (i == 4 || i == 9 || i == 14) { //any number other than a digit with the exception of hyphens at specific indexes
 				continue
 			}
-			return "Invalid5"
+			return "Invalid"
 		}
 	}
 
 	// Check if the card number contains 4 or more of same digit
 	for i := 0; i < len(cardNumber)-3; i++ {
 		if cardNumber[i] == cardNumber[i+1] && cardNumber[i] == cardNumber[i+2] && cardNumber[i] == cardNumber[i+3] {
-			return "Invalid6"
+			return "Invalid"
 		}
 	}
 
